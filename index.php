1e4b2d44faa419c93f9465e31525765b3855331d
milimdali56 <milimdali@gmail.com>
2018-10-13 01:29:54 +0600

    first push

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Mili's first PHP page</h1>
+
+<?php
+
+for($i=0; $i<10; $i++){
+echo "Hello php <br>";
+}
+
+$txt = "toke";
+echo "Valobashi " . $txt . " Foad <br>";
+
+$txt = "Valobasi";
+echo "$txt toke Charchil<br>";
+
+$a=1;
+while( $a <= 10){
+    echo "$a . Hello PHP <br>";
+    $a++;
+}
+?>
+
+
+</body>
+</html> 
