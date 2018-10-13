05d9b8e302bff33084f0a1d84dd2e61aa398aa02
milimdali56 <milimdali@gmail.com>
2018-10-13 21:02:27 +0600

    basic functions done

--- index.php
+++ index.php
@@ -1,29 +1,26 @@
 <!DOCTYPE html>
 <html>
+<head>
+<meta charset="utf-8">
+<title>Simple PHP Calculator</title>
+</head>
 <body>
+ <?php 
+ $a = $_GET["num1"];
+ $b = $_GET["num2"];
+ $opa = $_GET["opa"];
 
-<h1>Mili's first PHP page</h1>
-
-<?php
-Function add()
-{
-
+ switch( $opa) {
+    case "+": echo "Result of addition = ";
+    echo $sum= $a + $b; break;
+    case "-": echo "Result of subtraction = ";
+    echo $sum= $a - $b; break;
+    case "*": echo "Result of multiplication = ";
+    echo $sum= $a * $b; break;
+    case "/": echo "Result of division = ";
+    echo $sum= $a / $b; break;
 }
-Function substract ()
-{
-
-}
-Function multiply()
-{
-
-}
-Function divide()
-{
-
-}
-
-?>
-
+ ?>
 
 </body>
 </html> 
